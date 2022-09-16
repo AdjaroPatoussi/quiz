f2f4ff26fca0061f14a4b65886874a419e80f295
edouard <ediadjaro710@gmail.com>
2022-09-16 08:08:15 +0000

    init

---
+++ lib/Question.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Question {
+  String question;
+  bool reponse;
+
+  // Question(String q, bool r) {
+  //   this.question = q;
+  //   this.reponse = r;
+  // }
+
+  Question(this.question, this.reponse);
+}
